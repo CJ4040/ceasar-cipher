dce631d7de3ffc73b643af5732252d4d06004953
CJ4040 <40000470+CJ4040@users.noreply.github.com>
2021-08-07 18:04:47 +0200

    Add function to try all shift key options

--- CeaserCipher.py
+++ CeaserCipher.py
@@ -46,7 +46,35 @@ def ceasarDecrypt(codedText, shiftKey) :
     print(decryptedText)
 
 
+def hackCeasarDecrypt(codedText) : #function to be used when shiftkey is not known
+    
+    encrypt_text = codedText.upper() # convert coded text to uppercase. It will make it easier to use unicode numbers afterwards.
+    shiftKey = 0
+    decryptedText = ""
+    my_dict = {}
+    
+    while shiftKey <= 26 : # iterate through total numbers in alphabet.
+        for c in encrypt_text :
 
+            if c.isupper() :
+                c_unicode = ord(c) # get the unicode for each of the charachters in the encrypted text
+                c_index = c_unicode - ord("A") # get the index of each charachter in the alphabet for each of the charachters in the ciphertext
+
+                d_newIndex = (c_index - shiftKey) % 26
+                d_newUnicode = d_newIndex + ord("A")
+
+                d_charachter = chr(d_newUnicode)
+                decryptedText += d_charachter
+
+            else : 
+                decryptedText += c # if character is not upper case, just return the character unchanged.
+
+            my_dict[shiftKey] = decryptedText
+        
+        shiftKey +=1 #add 1 to the shiftkey
+        decryptedText ="" # clear the string after use
+
+    print(my_dict)
 
 
 
